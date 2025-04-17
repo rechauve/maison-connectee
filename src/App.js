1137b11de6e68ea570f148874bb96468d0f4f177
Rémi <rechauve@gmail.com>
2025-04-17 22:45:34 +0200

    Remove unused BrowserRouter import from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Agenda from './pages/Agenda';
